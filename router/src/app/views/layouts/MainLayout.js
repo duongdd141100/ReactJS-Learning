e389411ae6181be4b959e64d0a1a54a8fa67fe34
Do Duc Duong <duongdd2@vingroup.net>
2023-03-13 15:52:25 +0700

    [update]

--- router/src/app/views/layouts/MainLayout.js
+++ router/src/app/views/layouts/MainLayout.js
@@ -1,16 +1,23 @@
-import { Link, Routes,Route, Navigate } from 'react-router-dom'
+import { Link, Routes,Route, Navigate, NavLink } from 'react-router-dom'
 import Menu from './Menu.js'
 import { useState } from 'react'
 import '../../css/MainLayout.css'
 
 export default function MainLayout(props) {
     const [menu, setMenu] = useState([]);
+    const [activeLink, setActiveLink] = useState('/home')
+    
     return (
         <div>
             <div className='header'>
-                {props.items.map((item) => <Link key={item.path}
+                {props.items.map((item) => <NavLink
+                                                className={({ isActive }) => isActive ? "active-header-link" : "deactive-header-link"}
+                                                key={item.path}
                                                 onClick={() => setMenu(props.options.filter((option) => option.path.startsWith(item.path)))}
-                                                to={item.path}>{item.name}</Link>)}
+                                                to={item.path} >
+                                                {item.name}
+                                            </NavLink>)}
+                                            
             </div>
             <div className='body'>
                 <div className='menu'>
@@ -20,9 +27,6 @@ export default function MainLayout(props) {
                     <Routes>
                         <Route path='/' element={<Navigate to="/home" replace />}></Route>
                         {props.items.concat(props.options).map((item) => {
-                            if (item.path === '/home') {
-                                return <Route  key={item.path} index path={item.path} element={item.components} />
-                            }
                             return <Route key={item.path} path={item.path} element={item.components} />
                         })}
                     </Routes>
